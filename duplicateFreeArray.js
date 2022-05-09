7b8f07eff981cc91a39b74a2253900585dfec639
PrinceMargaret <princeagrahari2000@gmail.com>
2022-05-10 01:23:10 +0530

    first commit

---
+++ duplicateFreeArray.js
@@ -0,0 +1,11 @@
+const duplicateFreeArray = (arr) => {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (result.indexOf(arr[i]) === -1) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+
+console.log(duplicateFreeArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
